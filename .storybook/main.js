b124d4e200aee88adda1ccbe9fca77fcfc937c07
Julian Mendonca <julian@capicua.com.uy>
2022-04-22 01:02:23 -0300

    Trying to fix storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
     stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
-    staticDirs: ['../public'],
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-essentials',
