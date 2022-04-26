f0fe79c1b0d942aab4eb3a793889740baf0c52eb
Julian Mendonca <julian@capicua.com.uy>
2022-04-26 19:09:12 -0300

    Fixed deploy?

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Title from '@/components/Text/Title'
 import { Categoria } from '@/shared/Interfaces'
 import NavBar from '@/components/NavBar/NavBar'
 import { useGetCategories } from '@/shared/requests'
-import { environment } from '@/shared/constants'
 const Home = () => {
     const { data: categories } = useGetCategories()
     return (
@@ -17,7 +16,9 @@ export default Home
 
 export async function getStaticProps() {
     // `getStaticProps` is executed on the server side.
-    const categories = (await fetch(environment.domain + '/api/categorias').then((res) => res.json())) as Categoria[]
+    const categories = (await fetch('http://localhost:3000' + '/api/categorias').then((res) =>
+        res.json()
+    )) as Categoria[]
 
     return {
         props: {
