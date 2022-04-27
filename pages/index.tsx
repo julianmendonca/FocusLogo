35a8e5cf859846888e71200e2e70f84bdaaef108
Julian Mendonca <julian@capicua.com.uy>
2022-04-26 22:29:03 -0300

    Prin Domain

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,7 @@ const Home = ({ categories }: HomeProps) => {
         <>
             <NavBar categories={categories || []} />
             FocusLogo
+            {environment.domain}
         </>
     )
 }
